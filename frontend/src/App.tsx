3032f58bb2a2563adc1b203ec465095a8f94196a
awaisfatehali <awaisfatehali@gmail.com>
2026-05-31 16:19:39 +0500

    fix tsconfig

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import { useEffect } from "react";
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import { ToastContainer, Bounce } from "react-toastify"; // ✅ import Bounce
