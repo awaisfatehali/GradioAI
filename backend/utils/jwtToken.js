463ecfc5cd85ea481f41202c3fb816078d344243
awaisfatehali <awaisfatehali@gmail.com>
2026-06-03 15:17:23 +0500

    added cookies settings

--- backend/utils/jwtToken.js
+++ backend/utils/jwtToken.js
@@ -1,17 +1,18 @@
-const sendToken = (user,ststusCode,res)=>{
-    const token = user.getJwtToken();
+const sendToken = (user, statusCode, res) => {
+  const token = user.getJwtToken();
 
-    // options for cookies
-    const options = {
-        expires : new Date(Date.now()+90*24*60*60*1000),
-        httpOnly: true,
-    }
+  const options = {
+    expires: new Date(Date.now() + 90 * 24 * 60 * 60 * 1000),
+    httpOnly: true,
+    sameSite: "none", // ✅
+    secure: true,     // ✅
+  };
 
-    res.status(ststusCode).cookie("token",token,options).json({
-        success:true,
-        user,
-        token,
-    });
+  res.status(statusCode).cookie("token", token, options).json({
+    success: true,
+    user,
+    token,
+  });
 };
 
 module.exports = sendToken;
