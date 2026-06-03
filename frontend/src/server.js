2eb76d8b665c90fcee3d0645e38fc0ef966cd78d
awaisfatehali <awaisfatehali@gmail.com>
2026-06-03 14:49:37 +0500

    added real backend link

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,2 +1,2 @@
-// export const Backend_url = "http://localhost:8000/api/v2";
-export const Backend_url = "agency-project-backend.vercel.app/api/v2";
+export const Backend_url = "http://localhost:8000/api/v2";
+export const Backend_url = "gradio-ai-bard-2egnlztzh-awais-projects-2992b0c2.vercel.app/api/v2";
