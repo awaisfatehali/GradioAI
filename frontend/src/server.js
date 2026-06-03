ff13243bbf8c24db1c1a143d4dbebf726f744ef6
awaisfatehali <awaisfatehali@gmail.com>
2026-06-03 15:10:19 +0500

    okok

--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,2 +1,2 @@
 // export const Backend_url = "http://localhost:8000/api/v2";
-export const Backend_url = "gradio-ai-bard.vercel.app/api/v2";
+export const Backend_url = "https://gradio-ai-bard.vercel.app/api/v2";
