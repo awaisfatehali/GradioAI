f256828864ab99ce9ad4bab61e7e95a1d5884b82
awaisfatehali <awaisfatehali@gmail.com>
2026-06-03 14:45:53 +0500

    fixed Cors issue

--- backend/app.js
+++ backend/app.js
@@ -14,7 +14,7 @@ app.use("/",express.static("uploads"));
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://gradio-ai-j8m7.vercel.app",
     credentials: true,
   })
 );
--- frontend/src/server.js
+++ frontend/src/server.js
@@ -1,1 +1,2 @@
-export const Backend_url = "http://localhost:8000/api/v2";
+// export const Backend_url = "http://localhost:8000/api/v2";
+export const Backend_url = "agency-project-backend.vercel.app/api/v2";
